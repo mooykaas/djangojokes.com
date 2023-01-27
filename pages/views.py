1e50b7e40f5c7a60522fce6026171aacffa6ad60
marielle ooykaas <mooykaas@gmail.com>
2023-01-27 11:43:50 +0100

    nieuwe

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+ template_name = 'pages/home.html'
+
+
+ class AboutUsView(TemplateView):
+  template_name ='pages/about_us.html'
 
 # Create your views here.
